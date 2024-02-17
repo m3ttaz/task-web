546f2647860b10dd866d42081a33258fad8280d3
Moataz Hamdy <159279089+m3ttaz@users.noreply.github.com>
2024-02-17 17:40:32 +0200

    Add files via upload

--- book_store/script.js
+++ book_store/script.js
@@ -1,3 +1,6 @@
-for(let i=0 ; i<=10 ; i+=2){
-    console.log(i);
+for(let i=1 ; i<=10 ; i++){
+    console.log('table number '+i);
+    for(let j=1 ; j<=10 ; j++){
+        console.log(i*j);
+    }
 }
