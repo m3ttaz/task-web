34ed04875caf541888389aa330e935998a49e711
Moataz Hamdy <159279089+m3ttaz@users.noreply.github.com>
2024-03-07 03:22:23 +0200

    Add files via upload

--- book_store/script.js
+++ book_store/script.js
@@ -1,6 +1,10 @@
-for(let i=1 ; i<=10 ; i++){
-    console.log('table number '+i);
-    for(let j=1 ; j<=10 ; j++){
-        console.log(i*j);
+// find , findLast , filter , map
+const fun1=(len)  =>{
+    const arr=[];
+    for(let i=0;i<len;i++){
+        let n=Math.floor(Math.random()*100000000)+10000000;
+        arr.push("010"+n);
     }
+    return arr;
 }
+console.log(fun1(50));
